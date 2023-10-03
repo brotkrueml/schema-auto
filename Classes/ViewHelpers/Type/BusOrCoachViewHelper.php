21521021c9d09cdd7f59be599b83edc7e667dfd1
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-10-03 17:32:34 +0200

    fix: define type property in view helper

--- Classes/ViewHelpers/Type/BusOrCoachViewHelper.php
+++ Classes/ViewHelpers/Type/BusOrCoachViewHelper.php
@@ -18,4 +18,5 @@ use Brotkrueml\Schema\Core\ViewHelpers\AbstractTypeViewHelper;
  */
 final class BusOrCoachViewHelper extends AbstractTypeViewHelper
 {
+    protected string $type = 'BusOrCoach';
 }
--- Classes/ViewHelpers/Type/CarUsageTypeViewHelper.php
+++ Classes/ViewHelpers/Type/CarUsageTypeViewHelper.php
@@ -18,4 +18,5 @@ use Brotkrueml\Schema\Core\ViewHelpers\AbstractTypeViewHelper;
  */
 final class CarUsageTypeViewHelper extends AbstractTypeViewHelper
 {
+    protected string $type = 'CarUsageType';
 }
